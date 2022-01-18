82a08939786df3de773dfcf26861d32acc9356ae
Rian <rianatgithub@gmail.com>
2022-01-19 00:07:23 +0700

    update settings.py

--- LKSCloudComputing/settings.py
+++ LKSCloudComputing/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'LKSTraining',
-        'USER': 'postgres',
+        'USER': 'admin',
         'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '5432'
